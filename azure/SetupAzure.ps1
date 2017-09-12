d3fcb02a55b867f35f40ab826e303ca30a4e7542
John O'Connor <john.oconnor@puppet.com>
2017-09-12 10:51:03 +0000

    (MAINT) Add Key Vault

--- azure/SetupAzure.ps1
+++ azure/SetupAzure.ps1
@@ -5,3 +5,7 @@ Install-Module AzureRM
 Get-Module -ListAvailable AzureRM
 
 Import-Module AzureRM
+
+
+# Key Vault
+New-AzureRmKeyVault -VaultName "WinOps2017Vault" -ResourceGroupName "winopsrglondon" -Location "UKSouth" -EnabledForDeployment -EnabledForTemplateDeployment
