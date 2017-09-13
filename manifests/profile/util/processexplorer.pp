a939b12de1460f5e624a0ed19256d2531b64e20d
John O'Connor <john.oconnor@puppet.com>
2017-09-13 17:25:06 +0100

    Test push to production

--- manifests/profile/util/processexplorer.pp
+++ manifests/profile/util/processexplorer.pp
@@ -22,5 +22,5 @@ class profile::util::processexplorer {
 		source  => 'C:/temp/processexplorer/procexp.exe',
 		require => Archive['C:/temp/processexplorer.zip'],
 	}
-	#
+	# Comment to force push
 }
