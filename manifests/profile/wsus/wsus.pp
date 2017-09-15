61f4ccc650c8280cd0e8f98e31b87de497555c9f
John O'Connor <john.oconnor@puppet.com>
2017-09-15 15:31:57 +0100

    Add WSUS Server

---
+++ manifests/profile/wsus/wsus.pp
@@ -0,0 +1,12 @@
+  
+  class profile::wsus::wsus {
+  
+    # Disable Windows automatic updates
+    class { 'wsus_client': 
+            server_url             => 'http://winops-wsus.uksouth.cloudapp.azure.com:8530',
+            auto_update_option     => "Scheduled",
+            scheduled_install_day  => "Sunday",
+            scheduled_install_hour => 2,
+            enable_status_server => true,
+    }
+  }
