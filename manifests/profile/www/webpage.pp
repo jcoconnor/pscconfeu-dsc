948ad74836f3b1cfeec0f2ad55734d9a9e17954f
John O'Connor <john.oconnor@puppet.com>
2017-09-15 13:41:10 +0100

    Fix template pointer

--- manifests/profile/www/webpage.pp
+++ manifests/profile/www/webpage.pp
@@ -1,6 +1,6 @@
 class profile::www::webpage {
   file { 'C:/inetpub/wwwroot/iisstart.htm':
     ensure  => file,
-    content => epp('win_essentials/iisstart.htm.epp'),
+    content => epp('iisstart.htm.epp'),
   }
 }
