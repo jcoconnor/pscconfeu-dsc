24fac6e1951fb9814afd7ccd96f5a3d759eb4d12
James Stocks <james.stocks@puppet.com>
2017-09-19 14:06:55 +0100

    Update wannacry_vulnerable.pp

--- manifests/profile/wannacry_vulnerable/wannacry_vulnerable.pp
+++ manifests/profile/wannacry_vulnerable/wannacry_vulnerable.pp
@@ -3,8 +3,7 @@ class profile::wannacry_vulnerable {
   # Instead use a dummy file
   if $::osfamily == 'windows' {
     File { 'C:\Users\puppet\wannacry.txt':
-      ensure => present,
-      content => 'pwned by wannacry',
+      ensure => absent,
     }
   }
 }
