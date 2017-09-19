601c2b0fcea4171855f373c5b486f68eeb893828
John O'Connor <john.oconnor@puppet.com>
2017-09-19 14:50:49 +0100

    New WSUS Server changes

--- manifests/profile/wsus/wsus.pp
+++ manifests/profile/wsus/wsus.pp
@@ -1,12 +1,14 @@
-  
+
   class profile::wsus::wsus {
-  
-    # Disable Windows automatic updates
-    class { 'wsus_client': 
+
+    # Enforce Windows Update to run every Sunday.
+
+    class { 'wsus_client':
             server_url             => 'http://winops-wsus:8530',
             auto_update_option     => "Scheduled",
             scheduled_install_day  => "Sunday",
-            scheduled_install_hour => 2,
-            enable_status_server => true,
+            scheduled_install_hour => "2pm",
+            enable_status_server   => true,
     }
+
   }
