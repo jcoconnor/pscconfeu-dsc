eb4066ba0ce9baa1f31aed9d9858862f468eb049
John O'Connor <john.oconnor@puppet.com>
2017-09-19 17:28:00 +0100

    Revert to non-wsus configuration

--- manifests/profile/wsus/wsus.pp
+++ manifests/profile/wsus/wsus.pp
@@ -2,12 +2,11 @@
   class profile::wsus::wsus {
 
     # Enforce Windows Update to run every Sunday.
-
     class { 'wsus_client':
             server_url             => 'http://winops-wsus:8530',
             auto_update_option     => "Scheduled",
             scheduled_install_day  => "Sunday",
-            scheduled_install_hour => 2,
+            scheduled_install_hour => "2pm",
             enable_status_server   => true,
     }
 
