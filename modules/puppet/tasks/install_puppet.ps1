a391e2fa26ab3cdbcda7bbc38b508fa2f4a8d246
John O'Connor <john.oconnor@puppet.com>
2017-11-20 15:14:04 +0000

    Change Puppet Install Command to install latest.

--- modules/puppet/tasks/install_puppet.ps1
+++ modules/puppet/tasks/install_puppet.ps1
@@ -1,1 +1,34 @@
-choco install -y puppet-agent
+function Download-File {
+    param (
+      [string]$url,
+      [string]$file
+     )
+      $downloader = new-object System.Net.WebClient
+      $downloader.Proxy.Credentials=[System.Net.CredentialCache]::DefaultNetworkCredentials;
+    
+      Write-Output "Downloading $url to $file"
+      $completed = $false
+      $retrycount = 0
+      $maxretries = 20
+      $delay = 10
+      while (-not $completed) {
+        try {
+          $downloader.DownloadFile($url, $file)
+          $completed = $true
+        } catch {
+            if ($retrycount -ge $maxretries) {
+                Write-Output "Max Attempts exceeded"
+                throw "Download aborting"
+            } else {
+                $retrycount++
+                Write-Output "Download Failed $retrycount of $maxretries - Sleeping $delay"
+                Start-Sleep -Seconds $delay
+            }
+        }
+    }
+}
+
+# Install Puppet Agent
+Download-File "https://downloads.puppetlabs.com/windows/puppet-agent-x64-latest.msi" $Env:TEMP\puppet-agent.msi
+Start-Process -Wait "msiexec" -PassThru -NoNewWindow -ArgumentList "/i $Env:TEMP\puppet-agent.msi /qn /norestart PUPPET_AGENT_STARTUP_MODE=automatic PUPPET_MASTER_SERVER=winopsmasterlondon"
+Write-Output "Installed Puppet Agent..."
