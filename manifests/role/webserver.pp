ef8abbe915a4e31106cc80662f6a52a78be4f3a1
John O'Connor <john.oconnor@puppet.com>
2017-12-04 20:54:11 +0000

    Adding lamp/wordpress definitions

--- manifests/role/webserver.pp
+++ manifests/role/webserver.pp
@@ -1,6 +1,22 @@
 #
 
 class role::webserver {
-  include ::profile::iis::iisservice
-  include ::profile::www::webpage
+  if ($::osfamily == "windows" ) {
+    include ::profile::iis::iisservice
+    include ::profile::www::webpage
+  }
+  else {
+    # Departing slighlyt from the roles and profiles model here 
+    # and simply including the relevant modules
+    Exec {
+        path => ["/usr/bin", "/bin", "/usr/sbin", "/sbin", "/usr/local/bin", "/usr/local/sbin"]
+      }
+    include bootstrap
+    include tools
+    include apache
+    include php
+    include php::pear
+    include php::pecl
+    include mysql
+  }
 }
