77b3f26a59b03702b7443e1a1d44ab79d03ae91b
John O'Connor <john.oconnor@puppet.com>
2017-12-05 13:24:09 +0000

    Further fixen

--- manifests/profile/db/db.pp
+++ manifests/profile/db/db.pp
@@ -1,6 +1,6 @@
 #
 class profile::db::db {
   class { 'mysql::server':
-    config_hash => { 'root_password' => '8ZcJZFHsvo7fINZcAvi0' }
+    root_password => '8ZcJZFHsvo7fINZcAvi0' 
   }
 }
--- manifests/role/wordpress.pp
+++ manifests/role/wordpress.pp
@@ -1,6 +1,6 @@
 #
 class role::wordpress {
-  include profile::db::db
-  include profile::db::php
-  include profile::wordpress::wordpress
+  include ::profile::db::db
+  include ::profile::db::php
+  include ::profile::wordpress::wordpress
 }
