bfea1ca4ff4036c4332ba7e4dbc3f8e06c4455d9
John O'Connor <john.oconnor@puppet.com>
2017-12-06 14:25:48 +0000

    Fix 2am error in wsus definition

--- manifests/profile/wsus/wsus.pp
+++ manifests/profile/wsus/wsus.pp
@@ -1,12 +1,13 @@
 
+  # Obligatory comment
   class profile::wsus::wsus {
 
     # Enforce Windows Update to run every Sunday.
     class { 'wsus_client':
             server_url             => 'http://winops-wsus:8530',
-            auto_update_option     => "Scheduled",
-            scheduled_install_day  => "Sunday",
-            scheduled_install_hour => 2am,
+            auto_update_option     => 'Scheduled',
+            scheduled_install_day  => 'Sunday',
+            scheduled_install_hour => 2,
             enable_status_server   => true,
     }
 
